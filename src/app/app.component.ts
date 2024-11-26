0ba435c188f577c7a95433dac0af01322598dc92
Carlos Eduardo <ferreiira.kadu@gmail.com>
2024-11-26 13:47:08 -0300

    chore: adicionando loading na base da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LoadingComponent} from './shared/loading/loading.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoadingComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss'
