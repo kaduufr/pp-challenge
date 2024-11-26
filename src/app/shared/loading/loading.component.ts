bda93b973aa9da9f58436ec8175a4ba9faa15d28
Carlos Eduardo <ferreiira.kadu@gmail.com>
2024-11-26 12:41:39 -0300

    refactor: alterado como o loading é exibido

--- src/app/shared/loading/loading.component.ts
+++ src/app/shared/loading/loading.component.ts
@@ -1,15 +1,26 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {LoadingService} from '../services/loading/loading.service';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {AsyncPipe, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-loading',
-  imports: [],
+  imports: [
+    NgIf,
+    AsyncPipe
+  ],
   standalone: true,
   templateUrl: './loading.component.html',
   styleUrl: './loading.component.scss'
 })
-export class LoadingComponent {
+export class LoadingComponent implements OnInit {
 
   @Input() size: string | number = 2;
-  constructor() {}
+  show: Observable<boolean> = new BehaviorSubject<boolean>(false);
+  constructor(private loadingService: LoadingService) {}
+
+  ngOnInit() {
+    this.show = this.loadingService.isLoading;
+  }
 
 }
